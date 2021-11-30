34c52bfc25e8e139931e510e66bcb2e625fbaafb
shouryacoded <shourya.online23@gmail.com>
2021-11-30 17:41:29 +0530

    Linked list update

---
+++ linked_list.cpp
@@ -0,0 +1,44 @@
+// This file is made for sole purpose of practise and nothing else in mind that at this point of time happens to be linked list.
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+        int num;
+        node* next;
+};
+
+void print(node* n)
+{
+    while(n != NULL)
+    {
+        cout << n->num << " " ;
+        n = n->next;
+    }
+}
+
+int main()
+{
+
+
+
+    node* head = new node();
+    node* second = new node();
+    node* third = new node();
+
+    head->num = 10;
+    head->next = second;
+
+    second->num = 20;
+    second->next = third;
+
+    third->num = 30;
+    third->next = NULL;
+
+    cout << (second->next)->num;
+
+    cout << "\n A function to print the nodes is called below :\n";
+    print(head);
+
+    return 0;
+}
