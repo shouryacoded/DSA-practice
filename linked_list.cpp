ee5b456e9a3c2a059eedf8a36ae751e624b6f744
shouryacoded <shourya.online23@gmail.com>
2022-02-12 11:20:45 +0530

    Changes in linked list

--- linked_list.cpp
+++ linked_list.cpp
@@ -68,6 +68,34 @@ void Append(Node** head_ref, int new_data)
     last->next = new_node;
     return;
 }
+
+void deleteNode(Node** head_ref, int key)
+{
+    Node* temp = new Node();
+    Node* prev = NULL;
+
+    if(temp != NULL && temp->data==key)
+    {
+        *head_ref = temp->next;
+        delete temp;
+        return;
+    }
+    else
+    {
+        while(temp != NULL && temp->data == key)
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+        if(temp != NULL)
+            return;
+        
+        prev->next = temp->next;
+
+        delete temp;
+    }
+}
+
 int main()
 {
     Node* head = NULL;
@@ -80,5 +108,14 @@ int main()
 
     printlist(head);
     cout << endl;
+
+
+    cout << "Delete a node after this line.";
+    cout << "\n We'll try deleting 10 from the list";
+    deleteNode(&head,10);
+
+    cout << "\n Trying to print the list after deleting the key from the list.";
+    printlist(head);
+
     return 0;
 }
