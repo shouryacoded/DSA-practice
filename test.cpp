9bdc7a1d56c4ec5f45ee20cb0d86a8ac04e73dfc
shouryacoded <shourya.online23@gmail.com>
2022-06-08 13:09:55 +0530

    Changes

--- test.cpp
+++ test.cpp
@@ -1,10 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int findDuplicates (vector<int> &nums)
+{
+    
+}
+
+void print(vector<int> &nums)
+{
+    for(int i : nums)
+        cout << i << " ";
+}
+
 int main()
 {
-    system("cls");  // how the testing of the modules
-    int t;
-    cout << 6%4; 
+//    vector<int> nums = {1,3,4,2,2};
+    vector<int> nums = {2,2,2,2,2};
+
+    int res = findDuplicates(nums);
+    cout << "Res : " << res;
+    cout << endl;
+    print(nums);
     return 0;
 }
-// Changes to check upload.
