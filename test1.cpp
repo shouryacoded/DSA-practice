213422c7c6ea6461c482897c4ecc70acd451ac33
shouryacoded <shourya.online23@gmail.com>
2022-06-26 16:57:01 +0530

    Updates:

--- test1.cpp
+++ test1.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main()
 {
-    cout << "HEllo " ;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int size;
+        int temp;
+        cin >> size;
+        unordered_set<int> st;
+        vector<int> ar;
+        for(int i = 0; i < size; i++){
+            cin >> temp;
+            ar.emplace_back(temp);
+            st.insert(temp);
+        }
+        if(st.size() == 1 && ar.size() > 1)
+            cout << 0 << endl;
+        if(st.size() == ar.size())
+            cout << ar.size()-1 << endl;
+        if(st.size() != ar.size() && st.size() != 1)
+            cout << ar.size() - st.size() << endl;
+    }
     return 0;
 }
